f40da368689782f82f43324f1047b8b93a554be4
Ankush Sharma <ankprashar@gmail.com>
2015-08-10 22:27:16 +0530

    initial commit

---
+++ api.js
@@ -0,0 +1,100 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Address",
+        "Addresses",
+        "ApiRequest",
+        "BaseRequest",
+        "Client",
+        "Domain",
+        "List",
+        "ListArchivers",
+        "Member",
+        "Page",
+        "Preferences",
+        "Settings",
+        "User"
+    ],
+    "modules": [
+        "Address",
+        "Addresses",
+        "ApiRequest",
+        "BaseRequest",
+        "Domain",
+        "List",
+        "ListArchivers",
+        "Member",
+        "Page",
+        "Preferences",
+        "Settings",
+        "User",
+        "mailman-client"
+    ],
+    "allModules": [
+        {
+            "displayName": "Address",
+            "name": "Address",
+            "description": "Address extends ApiRequest to perform operations on the address resource"
+        },
+        {
+            "displayName": "Addresses",
+            "name": "Addresses",
+            "description": "Addresses extends ApiRequest to perform operations on the Addresses resource"
+        },
+        {
+            "displayName": "ApiRequest",
+            "name": "ApiRequest",
+            "description": "ApiRequest extends BaseRequest with custom properties and methods"
+        },
+        {
+            "displayName": "BaseRequest",
+            "name": "BaseRequest",
+            "description": "BaseRequest is the base API request object constructor"
+        },
+        {
+            "displayName": "Domain",
+            "name": "Domain",
+            "description": "Domain extends ApiRequest to perform operations on the domain resource"
+        },
+        {
+            "displayName": "List",
+            "name": "List",
+            "description": "List extends ApiRequest to perform operations on the list resource"
+        },
+        {
+            "displayName": "ListArchivers",
+            "name": "ListArchivers",
+            "description": "ListArchivers extends ApiRequest to perform operation on the list archivers"
+        },
+        {
+            "displayName": "mailman-client",
+            "name": "mailman-client"
+        },
+        {
+            "displayName": "Member",
+            "name": "Member",
+            "description": "Member extends ApiRequest to perform operations on the member resource"
+        },
+        {
+            "displayName": "Page",
+            "name": "Page",
+            "description": "Page extends ApiRequest to provide pagination functionality"
+        },
+        {
+            "displayName": "Preferences",
+            "name": "Preferences",
+            "description": "Preferences extends ApiRequest to do operations on the preferences resource"
+        },
+        {
+            "displayName": "Settings",
+            "name": "Settings",
+            "description": "Settings extends ApiRequest to do operations on the settings resource"
+        },
+        {
+            "displayName": "User",
+            "name": "User",
+            "description": "User extends ApiRequest to perform operations on the user resource"
+        }
+    ]
+} };
+});
